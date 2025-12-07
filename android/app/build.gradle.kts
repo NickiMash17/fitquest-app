aa52685cc99b4687feb09d555d1b5526e61eb23f
Nicolette Mashaba <nicolette.mashaba@techbridlefoundation.org>
2025-12-07 20:11:27 +0200

    chore: update Android configuration
    - Update build.gradle.kts for production build
    - Update AndroidManifest.xml with proper permissions and metadata

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.fitquest"
+    namespace = "com.fitquest.app"
     compileSdk = flutter.compileSdkVersion
 
     compileOptions {
@@ -19,8 +19,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.fitquest"
+        applicationId = "com.fitquest.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
